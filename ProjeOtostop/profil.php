5f31445b03e808397d1926ac4ab4c08fbb4459b9
EdanurKlnc <77683001+EdanurKlnc@users.noreply.github.com>
2021-12-13 02:26:21 +0300

    Profil sekmesi

--- ProjeOtostop/profil.php
+++ ProjeOtostop/profil.php
@@ -7,6 +7,7 @@
     <title>Profil | OTOSTOP</title>
 
     <link rel="stylesheet" href="Css/profil.css"> 
+    <link rel="stylesheet" href="Css/style.css"> 
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -24,19 +25,21 @@
 <a href="afterloginabout.php"><i class="fas fa-info"></i>Hakkımızda</a> 
 <a href="afterlogincotact.php"><i class="fas fa-phone-volume"></i>Iletişim</a> 
 <a href="profil.php"><i class="fa fa-user-circle-o"></i>Profil</a> 
+<a href="anasayfa.php"><i class="fas fa-times-circle"></i></i>Çıkış Yap</a> 
 </section>
 
     </nav>
-    <div class="container" style="width: 100%; height: 100%;">
+    <div class="container" style="width: 100%; height: 40%;">
 
         <div class="image" style="width: 20%; height: 20%;">
 <div class="ekle">
-    <input type="submit" name="img-ekle" class="img" value="Fotoğraf Ekle">
+ 
+<input type="button" value="Fotoğraf Ekle" class="about-btn">
 
 </div>
 
 </div>
-<div class="content" style="width: 40%;height: 50%;">
+<div class="content" style="width: 30%;height: 40%;">
     <div class="kullanici" ><p name="kadi" class="icerik">Ad </p></div>
     <div class="kullanici" ><p name="ksoyadi" class="icerik">Soyad</p></div>
     <div class="kullanici" ><p name="karac" class="icerik">E-mail</p></div>
@@ -51,11 +54,6 @@
 
 
 </div>
-
-
-
-
-
 
     </div>
 
