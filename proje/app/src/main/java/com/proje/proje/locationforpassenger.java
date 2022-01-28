1f9c29c7ed85295665200377658618157525ee54
Serhat-Saat <s.s.serhatsaat12@gmail.com>
2022-01-28 11:14:56 +0300

    güncel hali

---
+++ proje/app/src/main/java/com/proje/proje/locationforpassenger.java
@@ -0,0 +1,14 @@
+package com.proje.proje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class locationforpassenger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locationforpassenger);
+    }
+}
